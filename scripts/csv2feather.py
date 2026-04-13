8a1eb460f6b1467a716d318006a2e52f1470bab4
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2026-04-13 14:04:24 +0300

    Add Python dependecency manager and git hooks
    Added githooks that lints Python and C++ code. Also added git hooks,
    that erases jupyter-notebook cells outputs before commiting in `.git` code base.
    Applied hooks on existed code.
    PDM dependency manager lets use same env for development and lock
    versions of packages.

--- scripts/csv2feather.py
+++ scripts/csv2feather.py
@@ -10,11 +10,13 @@ import sys
 import pandas as pd
 
 if len(sys.argv) < 2:
-    print(f"Usage: {sys.argv[0]} <input_csv_file> <output_feather_file>", file=sys.stderr)
+    print(
+        f"Usage: {sys.argv[0]} <input_csv_file> <output_feather_file>", file=sys.stderr
+    )
     exit(1)
 
-csv_file = sys.argv[1] 
-fth_file = sys.argv[2] 
+csv_file = sys.argv[1]
+fth_file = sys.argv[2]
 
 data = pd.read_csv(csv_file)
-data.to_feather(fth_file, compression='lz4')
+data.to_feather(fth_file, compression="lz4")
--- test/TempFile.hpp
+++ test/TempFile.hpp
@@ -8,55 +8,42 @@
 
 namespace cmf {
 
-class TempFile
-{
+class TempFile {
 private:
-    std::filesystem::path filename_;
-    std::filesystem::path path_;
+  std::filesystem::path filename_;
+  std::filesystem::path path_;
 
 public:
-    template <class File>
-    TempFile(const File& filename) :
-        filename_{filename},
-        path_{std::filesystem::temp_directory_path() / filename}
-    {
-        // Remove any existing file
-        if (std::filesystem::exists(path_))
-            std::filesystem::remove(path_);
+  template <class File>
+  TempFile(const File &filename)
+      : filename_{filename},
+        path_{std::filesystem::temp_directory_path() / filename} {
+    // Remove any existing file
+    if (std::filesystem::exists(path_))
+      std::filesystem::remove(path_);
+  }
+
+  ~TempFile() { std::filesystem::remove(path_); }
+
+  std::filesystem::path getFilename() const { return filename_; }
+
+  std::filesystem::path getPath() const { return path_; }
+
+  std::string getContents() const {
+    std::string contents, line;
+    std::ifstream fs(path_);
+
+    bool first = true;
+    while (std::getline(fs, line)) {
+      if (first)
+        first = false;
+      else
+        contents += "\n";
+
+      contents += line;
     }
-
-    ~TempFile()
-    {
-        std::filesystem::remove(path_);
-    }
-
-    std::filesystem::path getFilename() const
-    {
-        return filename_;
-    }
-
-    std::filesystem::path getPath() const
-    {
-        return path_;
-    }
-
-    std::string getContents() const
-    {
-        std::string contents, line;
-        std::ifstream fs(path_);
-
-        bool first = true;
-        while (std::getline(fs, line))
-        {
-            if (first)
-                first = false;
-            else
-                contents += "\n";
-
-            contents += line;
-        }
-        return contents;
-    }
+    return contents;
+  }
 };
 
-}
+} // namespace cmf
